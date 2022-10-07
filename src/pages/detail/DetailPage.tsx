1157dbcd780228bfd26dc80283ed60ce37178eb7
lisong <1104634829@qq.com>
2022-10-07 20:35:28 +0800

    第二次提交

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -3,5 +3,5 @@ import { useParams } from "react-router-dom";
 
 export const DetailPage: React.FC<any> = (props) => {
     const { touristRouteId } = useParams<{touristRouteId:string}>();
-    return <h1>路游路线详情页面, 路线ID:{touristRouteId}</h1>;
+    return <h1>路游路线详情页面, 路线123ID:{touristRouteId}</h1>;
 };
